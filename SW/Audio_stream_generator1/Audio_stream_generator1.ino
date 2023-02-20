bced6b9d7abe42a5401194a205b2e19f37a619c6
immortal_sniper1 <38986383+immortal-sniper1@users.noreply.github.com>
2023-02-20 11:14:15 +0200

    Update Audio_stream_generator1.ino

--- SW/Audio_stream_generator1/Audio_stream_generator1.ino
+++ SW/Audio_stream_generator1/Audio_stream_generator1.ino
@@ -10,7 +10,7 @@
 
 uint16_t sample_rate=22000;
 uint8_t channels = 2;                                      // The stream will have 2 channels 
-SineWaveGenerator<int16_t> sineWave(5000);                // subclass of SoundGenerator with max amplitude of 32000
+SineWaveGenerator<int16_t> sineWave(10000);                // subclass of SoundGenerator with max amplitude of 32000
 GeneratedSoundStream<int16_t> sound(sineWave);             // Stream generated from sine wave
 I2SStream out; 
 StreamCopy copier(out, sound);                             // copies sound into i2s
